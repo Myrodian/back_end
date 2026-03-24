4300de64d965f8e29db6cb50fe9380f53383fa14
Augusto Leal <augustoleal11@gmail.com>
2026-03-24 10:46:44 -0300

    06 - implementando pageable

--- src/main/java/br/ifmg/produto1_2026/resources/categoriaResource.java
+++ src/main/java/br/ifmg/produto1_2026/resources/categoriaResource.java
@@ -5,10 +5,13 @@ import br.ifmg.produto1_2026.entities.Categoria;
 import br.ifmg.produto1_2026.service.CategoriaService;
 import br.ifmg.produto1_2026.service.exception.RegistroNaoEncontrado;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -22,14 +25,15 @@ public class categoriaResource {
     private CategoriaService categoriaService;
 
     @GetMapping
-    public ResponseEntity<List<CategoriaDTO>> categorias(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "sort", defaultValue = "id") String sort
+    public ResponseEntity<Page<CategoriaDTO>> categorias(
+//            @RequestParam(value = "page", defaultValue = "0") Integer page,
+//            @RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+//            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+//            @RequestParam(value = "sort", defaultValue = "id") String sort
+            Pageable pageable
     ) {
 
-        List<CategoriaDTO> categorias = categoriaService.findAll();
+        Page<CategoriaDTO> categorias = categoriaService.findAll(pageable);
 
         return ResponseEntity.ok().body(categorias);
     }
--- src/main/java/br/ifmg/produto1_2026/service/CategoriaService.java
+++ src/main/java/br/ifmg/produto1_2026/service/CategoriaService.java
@@ -8,6 +8,8 @@ import br.ifmg.produto1_2026.service.exception.RegistroNaoEncontrado;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,18 +26,18 @@ public class CategoriaService {
     private CategoriaRepository categoriaRepository;
 
     @Transactional(readOnly = true)
-    public List<CategoriaDTO> findAll() {
+    public Page<CategoriaDTO> findAll(Pageable pageable) {
         // lista com os dados do BD.
-        List<Categoria> categorias = categoriaRepository.findAll();
+        Page<Categoria> categorias = categoriaRepository.findAll(pageable);
         // lista com os dados convertidos em DTO ( por que DTO? porque você pode não querer trafegar tudo que está na entity, então vc "filtra" a entity,
         // esse "filtro" se chama DTO
-        List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
+//        List<CategoriaDTO> categoriasDTO = new ArrayList<CategoriaDTO>();
 
 //        for (Categoria categoria : categorias){
 //            categoriasDTO.add(new CategoriaDTO(categoria));
 //        }
 
-        return categorias.stream().map(x -> new CategoriaDTO(x)).collect(Collectors.toList());
+        return categorias.map(CategoriaDTO::new);
 
 //        return categoriasDTO;
     }
